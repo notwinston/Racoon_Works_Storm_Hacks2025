a2998bbfd3cc037adfb861c6deb29304987cb8a3
Dennis Kritchko <dennis_kritchko@sfu.ca>
2025-10-04 13:58:47 -0700

    added cmakelists

---
+++ build/CMakeFiles/algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithms.dir/equations/algorithms.cpp.o"
+  "CMakeFiles/algorithms.dir/equations/algorithms.cpp.o.d"
+  "algorithms"
+  "algorithms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
